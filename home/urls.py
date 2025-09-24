64f8b4f95948e41527074364ab5c96bce47b323b
Masoud <msd.mrdn01@gmail.com>
2025-09-25 01:26:35 +0330

    created templates and used "render" instead of "Httpresponse"

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     # path(url, view),
+    path('', views.home_page),
     path('hello/', views.say_hello),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+
+def home_page(request) :
+    return render(request, 'home.html')
+
+
 def say_hello(request) :
-    return HttpResponse("Hello world!")
+    return render(request, "hello.html")
