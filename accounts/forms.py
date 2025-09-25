8eadce7268009a60331357805a467c70fd32da96
Masoud <msd.mrdn01@gmail.com>
2025-09-25 11:10:35 +0330

    added user authentication using Django’s built-in User model

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class UserRegistrationForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
---
+++ accounts/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def user_register(request):
+    if request.method == "POST" :
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            user.first_name = cd['first_name']
+            user.last_name = cd['last_name']
+            user.save()
+            messages.success(request, 'User registered successfully', extra_tags='success')
+            return redirect('homepage')
+    else :
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {"form":form})
+
+
+def user_login(request):
+    if request.method == "POST" :
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None :
+                login(request, user)
+                messages.success(request, 'user logged in successfully', extra_tags='success')
+                return redirect('homepage')
+            else:
+                messages.error(request, 'Information does not match!', extra_tags='danger')
+    else :
+        form = UserLoginForm()
+    return render(request, 'login.html', {"form":form})
+
+
+def user_logout(request):
+    if request.user.is_authenticated :
+        logout(request)
+        messages.success(request, 'User logged out successfully', extra_tags='success')
+        return redirect('homepage')
+    else :
+        messages.error(request, 'You need to login first :)', extra_tags='warning')
+        return redirect('homepage')
