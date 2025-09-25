2dd911682afdaa26a4f1ce5722e91a73457ed09a
Masoud <msd.mrdn01@gmail.com>
2025-09-25 06:57:13 +0330

    added detail page and delete button

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     # path(url, view),
-    path('', views.home_page),
-    path('hello/', views.say_hello),
+    path('', views.home_page, name="homepage"),
+    path('hello/', views.say_hello, name="hellopage"),
+    path('detail/<int:record_id>', views.detail, name="detailpage"),
+    path('delete/<int:record_id>', views.delete, name="deletetodo")
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 def home_page(request) :
@@ -8,3 +8,12 @@ def home_page(request) :
 
 def say_hello(request) :
     return render(request, "hello.html")
+
+
+def detail(request, record_id) :
+    record = Todo.objects.get(id=record_id)
+    return render(request, "detail.html", {"r":record})
+
+def delete(request, record_id) :
+    Todo.objects.get(id=record_id).delete()
+    return redirect("homepage")
